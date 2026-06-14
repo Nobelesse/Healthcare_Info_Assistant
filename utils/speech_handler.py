799e7b5e12a6e11877fb5b1acbc4df39e4755a6b
Nobelesse <adityaprince1000@gmail.com>
2026-06-14 15:26:10 +0530

    Add free voice assistant using Faster Whisper and gTTS

--- utils/speech_handler.py
+++ utils/speech_handler.py
@@ -1,44 +1,40 @@
 import tempfile
 
-from openai import OpenAI
 from gtts import gTTS
+from faster_whisper import WhisperModel
 
-import streamlit as st
 
-
-client = OpenAI(
-    api_key=st.secrets["OPENAI_API_KEY"]
+model = WhisperModel(
+    "base",
+    device="cpu",
+    compute_type="int8"
 )
 
 
 def save_audio(audio_bytes):
-
     temp_audio = tempfile.NamedTemporaryFile(
         delete=False,
         suffix=".wav"
     )
 
     temp_audio.write(audio_bytes)
-
     temp_audio.close()
 
     return temp_audio.name
 
 
 def speech_to_text(audio_path):
+    segments, info = model.transcribe(audio_path)
 
-    with open(audio_path, "rb") as audio_file:
+    text = " ".join(
+        segment.text
+        for segment in segments
+    )
 
-        transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio_file
-        )
-
-    return transcript.text
+    return text.strip()
 
 
 def text_to_speech(text):
-
     tts = gTTS(
         text=text,
         lang="en"
@@ -49,8 +45,8 @@ def text_to_speech(text):
         suffix=".mp3"
     )
 
-    tts.save(
-        temp_mp3.name
-    )
+    temp_mp3.close()
+
+    tts.save(temp_mp3.name)
 
     return temp_mp3.name
